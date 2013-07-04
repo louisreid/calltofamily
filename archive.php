2da989c4857642f14c0494c5b0e57ed766ff6156
Louis <louisreid@gmail.com>
2013-07-04 01:04:28 +0100

    archive template

--- archive.php
+++ archive.php
@@ -13,17 +13,7 @@ get_header(); ?>
 
 			<h2>Archives by Title:</h2>
 			<ul>
-			    <?php wp_get_archives('type=alpha');?>
-			</ul>
-
-			<h2>Archives by Month:</h2>
-			<ul>
-			    <?php wp_get_archives('type=monthly'); ?>
-			</ul>
-
-			<h2>Archives by Subject:</h2>
-			<ul>
-			    <?php wp_list_categories('title_li='); ?>
+			    <?php wp_get_archives(array( 'type' => 'monthly'));?>
 			</ul>
 
 			<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
