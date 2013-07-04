4e328a32ac440589490a3f1eb186a171046da17b
Louis <louisreid@gmail.com>
2013-07-04 01:05:14 +0100

    archive template

--- archive.php
+++ archive.php
@@ -8,12 +8,10 @@ get_header(); ?>
 		<div class="span12">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="post-page" id="post-<?php the_ID(); ?>">
-			<h1><?php the_title(); ?></h1>
-			<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 
 			<h2>Archives by Title:</h2>
 			<ul>
-			    <?php wp_get_archives(array( 'type' => 'monthly'));?>
+			    <?php wp_get_archives('type=alpha');?>
 			</ul>
 
 			<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
